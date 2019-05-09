3b97769a5b25b0abecd65f37cdd3989708c7388e
Tomoyuki Sakurai <y@trombik.org>
2019-05-09 19:29:25 +0900

    bugfix: update to 11.2 and 12.0

--- tests/integration/build_packages/spec/default_spec.rb
+++ tests/integration/build_packages/spec/default_spec.rb
@@ -4,7 +4,7 @@ context "after provision finishes" do
   [:server1, :server2].each do |sym|
     describe server(sym) do
       it "builds ports-mgmt/portmaster" do
-        result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 10_3 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
+        result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 11_2 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
         expect(result).to match(/SUCCESSFULL_BIULD/)
       end
     end
